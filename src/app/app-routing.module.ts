3a9854e3b60e255187ee1050551683dd1baea0bd
Portfolio <gretacombold@gmail.com>
2022-08-17 22:28:58 -0300

    yes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { IdiomasComponent } from './components/idiomas/idiomas.component';
 const routes: Routes = [
   {path:'experiencia',component: ExperienciaComponent},
   {path:'proyectos',component: ProyectosComponent},
-  {path:'educacion',component:EducacionComponent}
+  {path:'educacion',component:EducacionComponent},
+  {path: 'skills',component: SkillsComponent},
+  {path: 'idiomas', component: IdiomasComponent},
 ];
 
 @NgModule({
